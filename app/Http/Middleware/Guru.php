bd508423a4fe2ec502a1a0616b60cae82e61228b
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-08-13 17:21:14 +0800

    set auth for role guru

---
+++ app/Http/Middleware/Guru.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Guru
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->check() && auth()->user()->guru != Null){
+            return $next($request);
+        }
+
+            return redirect('login')->with('error','You don\'t have admin access');
+    }
+}
