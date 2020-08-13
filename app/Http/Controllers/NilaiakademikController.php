ecd683ec488b96c913bafac005e3c9b87635eaf0
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-08-13 16:43:25 +0800

    Create NilaiakademikController.php

---
+++ app/Http/Controllers/NilaiakademikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Nilaiakademik;
+use Illuminate\Http\Request;
+
+class NilaiakademikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Nilaiakademik  $nilaiakademik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nilaiakademik $nilaiakademik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Nilaiakademik  $nilaiakademik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nilaiakademik $nilaiakademik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Nilaiakademik  $nilaiakademik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nilaiakademik $nilaiakademik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Nilaiakademik  $nilaiakademik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nilaiakademik $nilaiakademik)
+    {
+        //
+    }
+}
