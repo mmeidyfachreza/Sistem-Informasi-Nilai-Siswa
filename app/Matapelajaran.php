3026caa66a21242935bff7d9469c6fa726c0f7d7
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-08-14 19:38:07 +0800

    config relation

--- app/Matapelajaran.php
+++ app/Matapelajaran.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Matapelajaran extends Model
 {
     protected $table = 'matapelajaran';
-    protected $fillable = ['nama','semester','idguru'];
+    protected $fillable = ['nama','semester','guru_id'];
 
     public function raport()
     {
         return $this->hasMany('App\Raport');
+    }
+
+    public function guru()
+    {
+        return $this->belongsTo('App\Guru','guru_id','matapelajaran_id');
     }
 }
