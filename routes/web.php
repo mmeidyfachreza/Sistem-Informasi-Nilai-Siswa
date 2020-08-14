0e499233732149e7e53e7c10f1fbdcb12db59184
Mei.dy <41539285+mmeidyfachreza@users.noreply.github.com>
2020-08-14 21:34:49 +0800

    add guru route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>'guru', 'prefix' => 'admin'], function () {
+    Route::resource('guru', 'GuruController');
+});
